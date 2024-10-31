177c35179f705b4a15da7ce7ab9e9093f928c259
Igor Gritsenko <igoryuha.g@gmail.com>
2024-10-31 05:30:27 +0300

    refactoring, improve provider

--- src/fastdi/graph.py
+++ src/fastdi/graph.py
@@ -30,46 +30,77 @@ def build_resolving_args(depends):
 
 class Provider:
 
-    def __init__(self):
-        self.graph = {}
+    def __init__(self, scope: str | None = None):
+        self.graph: dict[Any, AdjacentDependencies] = {}
+        self.scope = scope
 
-    def provide(self, scope: str, cache: bool = True):
-        def inner(provider):
-            p_signature = signature(provider)
+    def provide(
+            self,
+            origin: Callable[..., Any] | None = None,
+            *,
+            scope: str | None = None,
+            cache: bool = True,
+    ):
+        scope = scope if scope else self.scope
+        if not scope:
+            raise RuntimeError
 
-            depends = {}
-            for k, v in p_signature.parameters.items():
-                depends[k] = v.annotation
-
-            key_type = p_signature.return_annotation
-
-            if isgeneratorfunction(provider):
-                key_type, = get_args(key_type)
-                body_template = GEN_TEMPLATE
-            elif isfunction(provider):
-                body_template = FN_TEMPLATE
-
-            _cache = CACHE if cache else ''
-
-            globs = {
-                'origin': provider,
-                'key_type': key_type,
-                **depends,
-            }
-            body = body_template.format_map({
-                'args': build_resolving_args(depends),
-                'cache': _cache,
-            })
-            compiled = compile(body, '<string>', 'exec')
-            exec(compiled, globs)
-            resolve = globs['resolve']
-
-            self.graph[key_type] = AdjacentDependencies(
-                resolve=resolve,
-                key_type_scope=scope,
+        if origin:
+            return self._register(
+                provider=origin,
+                scope=scope,
+                cache=cache,
             )
-            return provider
+
+        def inner(provider):
+            return self._register(
+                provider=provider,
+                scope=scope,
+                cache=cache,
+            )
         return inner
+
+
+    def _register(
+            self,
+            provider: Callable[..., Any],
+            scope: str,
+            cache: bool,
+    ):
+        p_signature = signature(provider)
+
+        depends = {}
+        for k, v in p_signature.parameters.items():
+            depends[k] = v.annotation
+
+        key_type = p_signature.return_annotation
+
+        if isgeneratorfunction(provider):
+            key_type, = get_args(key_type)
+            body_template = GEN_TEMPLATE
+        elif isfunction(provider):
+            body_template = FN_TEMPLATE
+
+        _cache = CACHE if cache else ''
+
+        globs = {
+            'origin': provider,
+            'key_type': key_type,
+            **depends,
+        }
+        body = body_template.format_map({
+            'args': build_resolving_args(depends),
+            'cache': _cache,
+        })
+        compiled = compile(body, '<string>', 'exec')
+        exec(compiled, globs)
+        resolve = globs['resolve']
+
+        self.graph[key_type] = AdjacentDependencies(
+            resolve=resolve,
+            key_type_scope=scope,
+        )
+        return provider
 
 
 class AdjacentDependencies:
